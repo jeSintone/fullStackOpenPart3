174b13ec6f6a68c3f8a11fad7783844eefb60c3f
Jesse Sintonen <sintonej.esse@gmail.com>
2025-02-20 14:24:38 +0200

    Changes made to min lenght of name

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (body.name === undefined) {
@@ -77,17 +77,18 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+    const {name, number} = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
-    
-    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+
+    Person.findByIdAndUpdate(
+        request.params.id, 
+        {name, number},
+        {new: true, runValidators: true, contect: 'query'}
+        )
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -146,6 +147,8 @@ const errorHandler = (error,request,response,next) => {
     console.error(error.message)
     if (error.name === "CastError") {
         return response.status(400).send({error: "malformatted id"})
+    } else if (error.name === "ValidationError") {
+        return response.status(400).send({error: error.message})
     }
     next(error)
 }

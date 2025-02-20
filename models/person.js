f809f6d5542c0818ae2149856ee8ce436fb2418b
Jesse Sintonen <sintonej.esse@gmail.com>
2025-02-20 14:43:41 +0200

    Tasks done up to 3.21

--- models/person.js
+++ models/person.js
@@ -24,6 +24,12 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      }
+    },
     required: true
   }
 })
